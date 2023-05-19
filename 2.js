c351dca1943df523a4d7d68ea6aa6878daf45172
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-05-19 19:49:59 +0200

    2

--- 2.js
+++ 2.js
@@ -1,1 +1,18 @@
+let count = 0;
 
+function cardCount(card) {
+  card = prompt("Task2. Enter card:");
+  if (["2", "3", "4", "5", "6"].includes(card)) {
+    count += 1;
+  } else if (["10", "J", "Q", "K", "A"].includes(card)) {
+    count -= 1;
+  }
+
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+}
+
+alert(cardCount());
